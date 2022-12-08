9fc83ae3b0c8120107fd73c1df77e2049cbecb4d
André Lopes <andrelneres@hotmail.com>
2022-12-08 11:31:23 -0300

    Adaptação no SQLServer e primeira migration

--- SiteDeVendasMVC/Startup.cs
+++ SiteDeVendasMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace SiteDeVendasMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SiteDeVendasMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SiteDeVendasMVCContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("SiteDeVendasMVCContext"), builder =>
+                        builder.MigrationsAssembly("SiteDeVendasMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
