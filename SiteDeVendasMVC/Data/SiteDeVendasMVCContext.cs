255db8c282312dc6ad4573b2b53dce3ee9f8b767
André Lopes <andrelneres@hotmail.com>
2022-12-08 10:43:33 -0300

    CRUD Scaffolding

---
+++ SiteDeVendasMVC/Data/SiteDeVendasMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SiteDeVendasMVC.Models
+{
+    public class SiteDeVendasMVCContext : DbContext
+    {
+        public SiteDeVendasMVCContext (DbContextOptions<SiteDeVendasMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SiteDeVendasMVC.Models.Department> Department { get; set; }
+    }
+}
--- SiteDeVendasMVC/Startup.cs
+++ SiteDeVendasMVC/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SiteDeVendasMVC.Models;
 
 namespace SiteDeVendasMVC
 {
@@ -33,6 +35,9 @@ namespace SiteDeVendasMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SiteDeVendasMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SiteDeVendasMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
